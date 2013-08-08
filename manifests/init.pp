2cb95fc1fad057b3bafb851c18e2d33b58738b47
Christopher Petersen <chris@petersen.io>
2013-08-07 17:31:01 -0700

    Initial commit

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,11 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+# Public: Install the Eye-Fi app from a dmg file.
+#
+# Examples
+#
+#   include eyefi
+class eyefi {
+  package { 'Eye-Fi':
+    provider => 'pkgdmg',
+    source   => 'http://support.eye.fi/agent/download/3.4.29/Eye-Fi.dmg'
+  }
 }
